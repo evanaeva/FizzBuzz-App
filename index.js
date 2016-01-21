3dd780740ae96a14975d486a8357d79fd967460f
MST FARDOUES <MST FARDOUES>
2016-01-21 12:08:51 -0600

    modified FizzBuzz

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 $(document).ready(function(){
-		
+	var ghor = prompt('please enter a number');
+	var anghor = +ghor;	
 	
-	for(var i=1; i<=15; i++){
+	var whole= function(){
+		for(var i = 1; i <= anghor;  i++){
 		
-		if(i%3 == 0 && i%5 ==0){console.log('Fizz Buzz');}
-   else if(i%3 == 0){console.log('Fizz');}
-   else if(i%5 == 0){console.log('Buzz');}
-   				else{console.log(i);}
+			if((i % 3 == 0) && (i % 5 == 0)){document.write (' Fizz Buzz ');}
+        	else if( i% 3 == 0){document.write (' Fizz ');}
+        	else if(i % 5 == 0){document.write (' Buzz ');}
+   			else {document.write ( i );}
 
-}
+		}
+	};
 
-
-
-
+ 		if(isNaN(anghor)){prompt('please enter a number');}
+ 		else if(anghor % 1 !==0){prompt('please enter a whole number');}
+ 		else{whole();}
 
 
 });
