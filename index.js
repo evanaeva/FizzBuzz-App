04b69d51f537d416ce3853551499ca3486ce2493
MST FARDOUES <MST FARDOUES>
2016-01-23 00:35:32 -0600

    add ParseInt and more modified

--- index.js
+++ index.js
@@ -1,21 +1,39 @@
 $(document).ready(function(){
-	var ghor = prompt('please enter a number');
-	var anghor = +ghor;	
-	
-	var whole= function(){
-		for(var i = 1; i <= anghor;  i++){
-		
-			if((i % 3 == 0) && (i % 5 == 0)){document.write (' Fizz Buzz ');}
-        	else if( i% 3 == 0){document.write (' Fizz ');}
-        	else if(i % 5 == 0){document.write (' Buzz ');}
-   			else {document.write ( i );}
+       var ghor = prompt('please enter a number');
+       var anghor = parseInt(ghor);  
+    
+       
+       var whole= function(){
+              for(var i = 1; i <= anghor;  i++){
+        
+                if((i % 3 == 0) && (i % 5 == 0))
+                    document.write (' Fizz Buzz ');
+                 
+                 else if( i% 3 == 0)
+                    document.write (' Fizz ');
+                 
+                 else if(i % 5 == 0)
+                    document.write (' Buzz ');
+                 
+                 else 
+                    document.write ( i + ' ' );
+                 
 
-		}
-	};
+              }
+       };
 
- 		if(isNaN(anghor)){prompt('please enter a number');}
- 		else if(anghor % 1 !==0){prompt('please enter a whole number');}
- 		else{whole();}
+        
+        if(isNaN(anghor))
+            prompt('please enter a number');
+        
+
+        else if(anghor % 1 !==0)
+            prompt('please enter a whole number');
+        
+        else
+            whole();
+        
 
 
 });
+
